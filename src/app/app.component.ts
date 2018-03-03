362c6f52d52bb57b2d724605fc7176369a32c969
harrydemo <harry.han@gmail.com>
2018-03-02 21:19:51 -0500

    git commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+
+/* harry import { Store } from '@ngrx/store';
+import { VersionCheckerService } from './core/services/version-checker.service';
+import { EchoesState } from '@store/reducers';
+import { getSidebarCollapsed, getAppTheme } from '@store/app-layout'; */
 
 @Component({
-  selector: 'app-root',
+  selector: 'body',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+ // harry sidebarCollapsed$ = this.store.select(getSidebarCollapsed);
+  // harry theme$ = this.store.select(getAppTheme);
+
+  @HostBinding('class')
+  style = 'arctic';
+
+ /* harry  constructor(private store: Store<EchoesState>, private versionCheckerService: VersionCheckerService) {
+    versionCheckerService.start();
+  } */
+
+  ngOnInit() {
+    // harry this.theme$.subscribe(theme => this.style = theme);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/* harry import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { CoreModule } from './core';
+import { SharedModule } from '@shared/index';
+import { APP_CORE_MODULES } from './core/components';
+import { APP_CONTAINER_MODULES } from './containers';
+import { ROUTES } from './app.routes'; */
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    /* harry FormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
+    RouterModule.forRoot(ROUTES, { useHash: true }),
+    BrowserAnimationsModule,
+
+    CoreModule,
+    SharedModule,
+    ...APP_CORE_MODULES,
+    ...APP_CONTAINER_MODULES */
   ],
   providers: [],
   bootstrap: [AppComponent]
